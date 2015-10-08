29b07b7185b939b9e24d9a8bef0ca5e0838dbefc
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2015-10-08 11:46:57 +0200

    create two scenarii with shared code + System.getProperty in single place

--- simulations/org/sonar/bdd/gatlingmysq/AdminChecksOutCeQueue.scala
+++ simulations/org/sonar/bdd/gatlingmysq/AdminChecksOutCeQueue.scala
@@ -8,9 +8,9 @@ import io.gatling.jdbc.Predef._
 
 class AdminChecksOutCeQueue extends Simulation {
 
-	val httpProtocol = http
-		.baseURL(System.getProperty("targetHost"))
-		.inferHtmlResources()
+    val uri1 = System.getProperty("targetHost")
+    var username = System.getProperty("sonar.username")
+    val password = System.getProperty("sonar.password")
 
 	val headers_0 = Map(
 		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -43,7 +43,7 @@ class AdminChecksOutCeQueue extends Simulation {
 	val headers_15 = Map(
 		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 		"Accept-Encoding" -> "gzip, deflate",
-		"Origin" -> System.getProperty("targetHost"),
+		"Origin" -> uri1,
 		"Upgrade-Insecure-Requests" -> "1")
 
 	val headers_18 = Map("accept" -> "application/json,*/*")
@@ -52,86 +52,118 @@ class AdminChecksOutCeQueue extends Simulation {
 		"Accept" -> "application/json, text/javascript, */*; q=0.01",
 		"X-Requested-With" -> "XMLHttpRequest")
 
-    val uri1 = System.getProperty("targetHost")
+	object Authenticate {
 
-	val scn = scenario("AdminChecksOutCeQueue")
+		val authenticate =
+			exec(
+				http("SQ Home").get("/").headers(headers_0)
+				.resources(
+					http("loading icon").get(uri1 + "/images/loading-small.gif")headers(headers_1),
+					http("sonar.css").get(uri1 + "/css/sonar.css?v=5.2-M57_2015-10-06").headers(headers_2),
+					http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06").headers(headers_3),
+					http("font").get(uri1 + "/fonts/Roboto-Regular-webfont.woff").headers(headers_3),
+	            	http("font").get(uri1 + "/fonts/sonar-5.2.woff?").headers(headers_3),
+					http("font").get(uri1 + "/fonts/Roboto-Light-webfont.woff").headers(headers_3),
+					http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06").headers(headers_3),
+					http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_8).check(status.is(304)),
+					http("favicon").get(uri1 + "/images/favicon.ico?1434375344").headers(headers_3),
+					http("logo").get(uri1 + "/images/logo.svg").headers(headers_10),
+					http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11)))
+			.pause(3)
+			.exec(
+				http("Go to login page").get("/sessions/new?return_to=%2F").headers(headers_12)
+				.resources(
+					http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
+					http("font").get(uri1 + "/fonts/Roboto-Medium-webfont.woff").headers(headers_3))
+				)
+			.pause(6)
+			.exec(
+				http("Login as admin").post("/sessions/login").headers(headers_15)
+				.formParam("return_to_anchor", "")
+				.formParam("login", username)
+				.formParam("password", password)
+				.formParam("remember_me", "1")
+				.formParam("commit", "")
+				.resources(
+					http("loading icon").get(uri1 + "/images/loading-small.gif").headers(headers_10),
+					http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
+					http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=49&metrics=ncloc&fields=name,longName,qualifier&pageSize=40&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
+					http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=54&metrics=coverage,ncloc&fields=name,longName,qualifier&pageSize=100&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
+					http("font").get(uri1 + "/fonts/Roboto-Bold-webfont.woff").headers(headers_3),
+					http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=32&metrics=ncloc,violations,line_coverage&fields=name,qualifier&pageSize=20&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
+					http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=38&metrics=coverage,ncloc&fields=name,longName,qualifier&pageSize=100&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
+					http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
+					http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
+					http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
+				)
+			.pause(5)
+	}
+
+	object BackgroundTaskPage {
+
+		val goTo =
+			exec(
+				http("go to Settings page").get("/settings").headers(headers_12)
+				.resources(
+					http("loading icon").get(uri1 + "/images/loading.gif").headers(headers_10),
+					http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
+					http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
+					http("/api/navigation/settings").get(uri1 + "/api/navigation/settings").headers(headers_11),
+					http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
+				)
+			.pause(4)
+			.exec(
+				http("Go to Background tasks page").get("/background_tasks").headers(headers_12)
+				.resources(
+					http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
+					http("background-tasks/app.js").get(uri1 + "/js/apps/background-tasks/app.js?v=5.2-M57_2015-10-06"),
+					http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
+					http("/api/ce/queue").get(uri1 + "/api/ce/queue").headers(headers_11),
+					http("/api/ce/activity").get(uri1 + "/api/ce/activity?p=1&ps=200").headers(headers_11),
+					http("/api/ce/activity?FAILED").get(uri1 + "/api/ce/activity?ps=1&onlyCurrents=true&status=FAILED").headers(headers_11),
+					http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
+					http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
+					http("/api/navigation/settings").get(uri1 + "/api/navigation/settings").headers(headers_11),
+					http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
+				)
+			.pause(5)
+
+		val reload =
+			exec(
+				http("Reload background tasks page").get("/api/ce/queue").headers(headers_11)
+				.resources(
+					http("/api/ce/activity?FAILED").get(uri1 + "/api/ce/activity?ps=1&onlyCurrents=true&status=FAILED").headers(headers_11),
+					http("/api/ce/activity").get(uri1 + "/api/ce/activity?p=1&ps=200").headers(headers_11))
+				)
+	}
+
+	val httpProtocol = http
+		.baseURL(uri1)
+		.inferHtmlResources()
+
+	val oneshot = scenario("Admin checks CE Queue once")
 		.exec(
-			http("SQ Home").get("/").headers(headers_0)
-			.resources(
-				http("loading icon").get(uri1 + "/images/loading-small.gif")headers(headers_1),
-				http("sonar.css").get(uri1 + "/css/sonar.css?v=5.2-M57_2015-10-06").headers(headers_2),
-				http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06").headers(headers_3),
-				http("font").get(uri1 + "/fonts/Roboto-Regular-webfont.woff").headers(headers_3),
-            	http("font").get(uri1 + "/fonts/sonar-5.2.woff?").headers(headers_3),
-				http("font").get(uri1 + "/fonts/Roboto-Light-webfont.woff").headers(headers_3),
-				http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06").headers(headers_3),
-				http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_8).check(status.is(304)),
-				http("favicon").get(uri1 + "/images/favicon.ico?1434375344").headers(headers_3),
-				http("logo").get(uri1 + "/images/logo.svg").headers(headers_10),
-				http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11)))
-		.pause(3)
+			Authenticate.authenticate,
+			BackgroundTaskPage.goTo,
+			BackgroundTaskPage.reload
+		)
+	var frenetic = scenario("Frenetic admin checks CE Queue")
 		.exec(
-			http("Go to login page").get("/sessions/new?return_to=%2F").headers(headers_12)
-			.resources(
-				http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
-				http("font").get(uri1 + "/fonts/Roboto-Medium-webfont.woff").headers(headers_3))
-			)
-		.pause(6)
-		.exec(
-			http("Login as admin").post("/sessions/login").headers(headers_15)
-			.formParam("return_to_anchor", "")
-			.formParam("login", System.getProperty("sonar.username"))
-			.formParam("password", System.getProperty("sonar.password"))
-			.formParam("remember_me", "1")
-			.formParam("commit", "")
-			.resources(
-				http("loading icon").get(uri1 + "/images/loading-small.gif").headers(headers_10),
-				http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
-				http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=49&metrics=ncloc&fields=name,longName,qualifier&pageSize=40&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
-				http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=54&metrics=coverage,ncloc&fields=name,longName,qualifier&pageSize=100&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
-				http("font").get(uri1 + "/fonts/Roboto-Bold-webfont.woff").headers(headers_3),
-				http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=32&metrics=ncloc,violations,line_coverage&fields=name,qualifier&pageSize=20&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
-				http("/measures/search_filter").get(uri1 + "/measures/search_filter?filter=38&metrics=coverage,ncloc&fields=name,longName,qualifier&pageSize=100&page=1&sort=metric:ncloc&asc=false").headers(headers_18),
-				http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
-				http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
-				http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
-			)
-		.pause(4)
-		.exec(
-			http("go to Settings page").get("/settings").headers(headers_12)
-			.resources(
-				http("loading icon").get(uri1 + "/images/loading.gif").headers(headers_10),
-				http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
-				http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
-				http("/api/navigation/settings").get(uri1 + "/api/navigation/settings").headers(headers_11),
-				http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
-			)
-		.pause(4)
-		.exec(
-			http("Go to Background tasks page").get("/background_tasks").headers(headers_12)
-			.resources(
-				http("sonar.js").get(uri1 + "/js/sonar.js?v=5.2-M57_2015-10-06"),
-				http("background-tasks/app.js").get(uri1 + "/js/apps/background-tasks/app.js?v=5.2-M57_2015-10-06"),
-				http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
-				http("/api/ce/queue").get(uri1 + "/api/ce/queue").headers(headers_11),
-				http("/api/ce/activity").get(uri1 + "/api/ce/activity?p=1&ps=200").headers(headers_11),
-				http("/api/ce/activity?FAILED").get(uri1 + "/api/ce/activity?ps=1&onlyCurrents=true&status=FAILED").headers(headers_11),
-				http("nav/app.js").get(uri1 + "/js/apps/nav/app.js?v=5.2-M57_2015-10-06"),
-				http("l10n").get(uri1 + "/api/l10n/index?locale=fr&ts=2015-10-07T15%3A06%3A31%2B0000").headers(headers_24).check(status.is(304)),
-				http("/api/navigation/settings").get(uri1 + "/api/navigation/settings").headers(headers_11),
-				http("/api/navigation/global").get(uri1 + "/api/navigation/global").headers(headers_11))
-			)
-		.pause(5)
-		.exec(
-			http("Reload background tasks page").get("/api/ce/queue").headers(headers_11)
-			.resources(
-				http("/api/ce/activity?FAILED").get(uri1 + "/api/ce/activity?ps=1&onlyCurrents=true&status=FAILED").headers(headers_11),
-				http("/api/ce/activity").get(uri1 + "/api/ce/activity?p=1&ps=200").headers(headers_11))
-			)
+			Authenticate.authenticate,
+			BackgroundTaskPage.goTo,
+			repeat(10) {
+				BackgroundTaskPage.reload
+				.pause(5)
+			}
+		)
 
 	setUp(
-		scn.inject(
+		oneshot.inject(
+			atOnceUsers(1)
+		),
+		frenetic.inject(
 			atOnceUsers(1)
 		)
+
 	).protocols(httpProtocol)
 }
